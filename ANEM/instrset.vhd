50dc39a514ec85dee9f949c81efc78c7496bb166
brunosmmm <brunosmmm@gmail.com>
2014-12-05 15:04:27 -0500

    Added opcode constants

---
+++ ANEM/instrset.vhd
@@ -0,0 +1,22 @@
+----------------------------------------------
+--! @file instrset.vhd
+--! @brief instruction set constants - OPCODES
+--! @author Bruno Morais <brunosmmm@gmail.com>
+--! @since 12/05/2014
+-----------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+package instrset is
+  constant ANEM_OPCODE_R   : std_logic_vector(3 downto 0) := "0000";
+  constant ANEM_OPCODE_S   : std_logic_vector(3 downto 0) := "0001";
+  constant ANEM_OPCODE_J   : std_logic_vector(3 downto 0) := "1000";
+  constant ANEM_OPCODE_JAL : std_logic_vector(3 downto 0) := "1001";
+  constant ANEM_OPCODE_BEQ : std_logic_vector(3 downto 0) := "0110";
+  constant ANEM_OPCODE_JR  : std_logic_vector(3 downto 0) := "0111";
+  constant ANEM_OPCODE_SW  : std_logic_vector(3 downto 0) := "0100";
+  constant ANEM_OPCODE_LW  : std_logic_vector(3 downto 0) := "0101";
+  constant ANEM_OPCODE_LIU : std_logic_vector(3 downto 0) := "1100";
+  constant ANEM_OPCODE_LIL : std_logic_vector(3 downto 0) := "1101";
+
+end package;
--- control/idecode.vhd
+++ control/idecode.vhd
@@ -7,7 +7,8 @@
 library ieee;
 
 use ieee.std_logic_1164.all;
-
+use work.instrset.all;
+  
 entity anem16_idecode is
 
   generic(instr_size : integer := 16 );
@@ -54,11 +55,11 @@ begin
   --controls ALU in ALU phase
   alu_ctl <= alu_ctl_0 when reset_detected = '0' else
              "000";
-  alu_ctl_0 <= "001" when opcode = "0000" else
-               "010" when opcode = "0001" else
-               "011" when opcode = "0110" else
-               "100" when opcode = "0100" else
-               "100" when opcode = "0101" else
+  alu_ctl_0 <= "001" when opcode = ANEM_OPCODE_S else
+               "010" when opcode = ANEM_OPCODE_R else
+               "011" when opcode = ANEM_OPCODE_BEQ else
+               "100" when opcode = ANEM_OPCODE_SW else
+               "100" when opcode = ANEM_OPCODE_LW else
                "000";
   
   alu_shamt <= instruction(7 downto 4) when alu_ctl_0 /= "000" else
@@ -71,38 +72,38 @@ begin
   --controls register writing on WB phase
   regbnk_ctl <= regbnk_ctl_0 when reset_detected = '0' else
                 "000";
-  regbnk_ctl_0 <= "010" when opcode = "1100" else --LIU INSTRUCTION
-                  "011" when opcode = "1101" else --LIL INSTRUCTION
-                  "001" when opcode = "0000" else --R TYPE INSTRUCTION
-                  "001" when opcode = "0001" else --S TYPE INSTRUCTION
-                  "100" when opcode = "0101" else --LW INSTRUCTION
+  regbnk_ctl_0 <= "010" when opcode = ANEM_OPCODE_LIU else --LIU INSTRUCTION
+                  "011" when opcode = ANEM_OPCODE_LIL else --LIL INSTRUCTION
+                  "001" when opcode = ANEM_OPCODE_R else --R TYPE INSTRUCTION
+                  "001" when opcode = ANEM_OPCODE_S else --S TYPE INSTRUCTION
+                  "100" when opcode = ANEM_OPCODE_LW else --LW INSTRUCTION
                   "000";
 
   regbnk_sela <= instruction(11 downto 8);
   regbnk_selb <= instruction(7 downto 4);
 
   --unconditional jumps
-  j_flag <= '1' when opcode = "1000" and reset_detected = '0' else
+  j_flag <= '1' when opcode = ANEM_OPCODE_J and reset_detected = '0' else
             '0';
   
-  j_dest <= "0000" & instruction(11 downto 0) when opcode = "1000" else
-            regbnk_aout                       when opcode = "0111" else
+  j_dest <= "0000" & instruction(11 downto 0) when opcode = ANEM_OPCODE_J else
+            regbnk_aout                       when opcode = ANEM_OPCODE_JR else
             (others=>'0');
 
-  jr_flag <= '1' when opcode = "0111" and reset_detected = '0' else
+  jr_flag <= '1' when opcode = ANEM_OPCODE_JR and reset_detected = '0' else
              '0';
 
-  beq_flag <= '1' when opcode = "0110" and reset_detected = '0' else
+  beq_flag <= '1' when opcode = ANEM_OPCODE_BEQ and reset_detected = '0' else
               '0';
-  beq_off  <= instruction(3 downto 0) when opcode = "0110" else
+  beq_off  <= instruction(3 downto 0) when opcode = ANEM_OPCODE_BEQ else
               "0000";
 
-  mem_en <= '1' when opcode = "0100" else
-            '1' when opcode = "0101" else
+  mem_en <= '1' when opcode = ANEM_OPCODE_SW else
+            '1' when opcode = ANEM_OPCODE_LW else
             '0' when reset_detected = '1' else
             '0';
 
-  mem_w <=  '1' when opcode = "0100" and reset_detected = '0' else
+  mem_w <=  '1' when opcode = ANEM_OPCODE_SW and reset_detected = '0' else
             '0';
 
   limmval <= instruction(7 downto 0);
